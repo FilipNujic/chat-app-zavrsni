d5fb4464eec97844386f30e6fedfdac43e557d8f
FilipNujic <filip.nujic1@gmail.com>
2023-02-14 16:05:28 +0100

    version 1.0

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useRef } from "react";
 import { auth } from "../firebase";
 
 const style = {
-  message: `flex items-center shadow-xl m-4 py-4 px-8 rounded-bl-full rounded-br-full`,
+  message: `flex items-center shadow-xl m-4 py-4 px-8 sm:rounded-bl-full sm:rounded-br-full`,
   name: `absolute inset-x-0 top-0 mt-[-1rem] text-accent-100 text-xl truncate`,
-  sent: `bg-accent-200 text-black flex-row-reverse text-end float-right rounded-tl-full`,
-  received: `bg-secondary-100 text-black text-start float-left rounded-tr-full`,
+  sent: `bg-accent-200 text-black flex-row-reverse text-end float-right max-w-full rounded-[60px] rounded-tr-none sm:rounded-tl-full`,
+  received: `bg-secondary-100 text-black text-start float-left max-w-full rounded-[60px] rounded-tl-none sm:rounded-tr-full`,
 };
 
 const Message = ({ message }) => {
@@ -23,7 +23,7 @@ const Message = ({ message }) => {
     <div className="relative" ref={scroll}>
       <div className={`${style.message} ${messageType}`}>
         <p className={style.name}>{message.name}</p>
-        <p className="text-start overflow-wrap break-all">{message.text}</p>
+        <p className="text-start overflow-hidden text-ellipsis break-words">{message.text}</p>
       </div>
     </div>
   );
